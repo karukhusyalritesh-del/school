50ce8cd563141dceff8336cc87fab2c97d889baa
ritesh tharu <karukhusyalritesh@gmail.com>
2025-10-07 07:43:30 +0545

    removed admin topNav

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import TopNav from './Components/TopNav'
 import NoticePage from './Components/add'
 
 
 const App = () => {
   return (
     <div>
-      <TopNav/>
       <NoticePage/>
     </div>
   )

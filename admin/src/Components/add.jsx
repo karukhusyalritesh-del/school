975e2fc34935b7accebcce1b9a5c75ff2e943de2
ritesh tharu <karukhusyalritesh@gmail.com>
2025-10-08 16:13:27 +0545

    cursor added

--- admin/src/Components/add.jsx
+++ admin/src/Components/add.jsx
@@ -200,7 +200,7 @@ const NoticePage = () => {
                 </button>
                 <button
                   type="submit"
-                  className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                  className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 cursor-pointer"
                 >
                   Save
                 </button>
